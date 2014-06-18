1af97a84cb859709b9e5887485493072d0a64115
jonathan poelen <jonathan.poelen@gmail.com>
2014-06-18 23:30:52 +0200

    cleaner code

--- bin/amarok_trace_file_and_next.sh
+++ bin/amarok_trace_file_and_next.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-/usr/lib/x86_64-linux-gnu/qt4/bin/qdbus org.kde.amarok /TrackList org.freedesktop.MediaPlayer.GetMetadata $(/usr/lib/x86_64-linux-gnu/qt4/bin/qdbus org.kde.amarok /TrackList org.freedesktop.MediaPlayer.GetCurrentTrack) | grep -m1 ^location | perl -pe 's#location: file://##,s|%([0-9a-f]{2})|pack('C',hex($1))|segi;' >> /home/jonathan/track_passed.txt
-/usr/lib/x86_64-linux-gnu/qt4/bin/qdbus org.mpris.MediaPlayer2.amarok /Player org.freedesktop.MediaPlayer.Next
+qdbus org.kde.amarok /TrackList org.freedesktop.MediaPlayer.GetMetadata \
+  $(qdbus org.kde.amarok /TrackList org.freedesktop.MediaPlayer.GetCurrentTrack) \
+| grep -m1 ^location \
+| perl -pe 's#location: file://##,s|%([0-9a-f]{2})|pack('C',hex($1))|segi;' \
+>> ~/track_passed.txt
+qdbus org.mpris.MediaPlayer2.amarok /Player org.freedesktop.MediaPlayer.Next
