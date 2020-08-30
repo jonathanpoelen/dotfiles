83cc92bca89a1f4670891777932d5dd40af3394a
Jonathan Poelen <jonathan.poelen@gmail.com>
2020-08-30 18:03:56 +0200

    [bin] perfgraph.sh: perf + flamegraph

---
+++ bin/perfgraph.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+FLAMEGRAPH=~/game/FlameGraph
+TMP="${TMPDIR:-/tmp}"
+SVG="$TMP"/perf.svg
+perf record --call-graph dwarf -o"$TMP/perf.data" "$@"
+perf script -i"$TMP/perf.data" |
+  "$FLAMEGRAPH"/stackcollapse-perf.pl |
+  "$FLAMEGRAPH"/flamegraph.pl > "$SVG"
+echo "$SVG"
