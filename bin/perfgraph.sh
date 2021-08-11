e244ff983dedce310e92f97403f0787d197ea413
Jonathan Poelen <jonathan.poelen@gmail.com>
2021-08-12 00:30:21 +0200

    [bin] perfgraph: add -r for remove previous recording file

--- bin/perfgraph.sh
+++ bin/perfgraph.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env sh
+if [[ $# = 0 ]]; then
+  echo "$0 [-r] perf-record-args..." >&2
+  echo '  -r: remove previous recording file' >&2
+  exit 1
+fi
+
 FLAMEGRAPH=~/game/FlameGraph
 TMP="${TMPDIR:-/tmp}"
 SVG="$TMP"/perf.svg
-perf record --call-graph dwarf -o"$TMP/perf.data" "$@"
-perf script -i"$TMP/perf.data" |
+DATA="$TMP"/perf.data
+if [[ "$1" = "-r" ]]; then
+  rm "$DATA" 2>/dev/null ||:
+  shift
+fi
+perf record --call-graph dwarf -o"$DATA" "$@"
+perf script -i"$DATA" |
   "$FLAMEGRAPH"/stackcollapse-perf.pl |
   "$FLAMEGRAPH"/flamegraph.pl > "$SVG"
 echo "$SVG" >&2
