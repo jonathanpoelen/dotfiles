1b8e7e5da42f6231590b7d2476403667ed93adc3
Jonathan Poelen <jonathan.poelen@gmail.com>
2023-06-03 01:18:05 +0200

    update dnsmasq-adblock scripts

--- dnsmasq/update-dnsmasq-adblock.sh
+++ dnsmasq/update-dnsmasq-adblock.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-rm /usr/lib/blocklists/{domains,hostnames}.txt ||:
-wget -P /usr/lib/blocklists https://raw.githubusercontent.com/notracking/hosts-blocklists/master/{domains,hostnames}.txt
+rm /usr/local/share/dnsmasq.blacklist.txt ||:
+wget -O /usr/local/share/dnsmasq.blacklist.txt https://github.com/notracking/hosts-blocklists/raw/master/dnsmasq/dnsmasq.blacklist.txt
 systemctl restart NetworkManager.service
 # or systemctl restart dnsmasq
